d7997009e045d747f8fbd6dbcb06f7a4229cefab
Evgeny Vodyannikov <vodyannikov.eugene@gmail.com>
2023-01-12 20:01:46 +0500

    add control buttons

--- src/components/AnimatedRectangle.tsx
+++ src/components/AnimatedRectangle.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback } from "react";
 
 interface RectangleProps {
   x: number;
@@ -7,17 +7,22 @@ interface RectangleProps {
   height: number;
 }
 
-const AnimatedRectangle: React.FC<RectangleProps> = ({ x, y, width, height }) => {
+const AnimatedRectangle: React.FC<RectangleProps> = ({
+  x,
+  y,
+  width,
+  height,
+}) => {
   const [rectangle, setRectangle] = useState({ x, y });
+  const [velocity, setVelocity] = useState({ x: 0, y: 0 });
 
   useEffect(() => {
     let animationFrameId: number;
-    let velocity = 1;
 
     function updateRectangle() {
-      setRectangle(prevRectangle => ({
-        x: prevRectangle.x + velocity,
-        y: prevRectangle.y + velocity,
+      setRectangle((prevRectangle) => ({
+        x: prevRectangle.x + velocity.x,
+        y: prevRectangle.y + velocity.y,
       }));
       animationFrameId = requestAnimationFrame(updateRectangle);
     }
@@ -25,19 +30,59 @@ const AnimatedRectangle: React.FC<RectangleProps> = ({ x, y, width, height }) =>
     animationFrameId = requestAnimationFrame(updateRectangle);
 
     return () => cancelAnimationFrame(animationFrameId);
+  }, [velocity]);
+
+  const handleKeyPress = useCallback((event: KeyboardEvent) => {
+    switch (event.key) {
+      case "w":
+        setVelocity((prevVelocity) => ({ ...prevVelocity, y: -1 }));
+        break;
+      case "a":
+        setVelocity((prevVelocity) => ({ ...prevVelocity, x: -1 }));
+        break;
+      case "s":
+        setVelocity((prevVelocity) => ({ ...prevVelocity, y: 1 }));
+        break;
+      case "d":
+        setVelocity((prevVelocity) => ({ ...prevVelocity, x: 1 }));
+        break;
+      default:
+        break;
+    }
   }, []);
 
+  useEffect(() => {
+    let animationFrameId: number;
+
+    function updateRectangle() {
+      setRectangle((prevRectangle) => ({
+        x: prevRectangle.x + velocity.x,
+        y: prevRectangle.y + velocity.y,
+      }));
+      animationFrameId = requestAnimationFrame(updateRectangle);
+    }
+
+    document.addEventListener("keydown", handleKeyPress);
+    animationFrameId = requestAnimationFrame(updateRectangle);
+
+    return () => {
+      cancelAnimationFrame(animationFrameId);
+      document.removeEventListener("keydown", handleKeyPress);
+    };
+  }, [velocity, handleKeyPress]);
+
   return (
-    <div style={{
-      position: "absolute",
-      left: rectangle.x,
-      top: rectangle.y,
-      width: width,
-      height: height,
-      backgroundColor: "red"
-    }}>
-    </div>
+    <div
+      style={{
+        position: "absolute",
+        left: rectangle.x,
+        top: rectangle.y,
+        width: width,
+        height: height,
+        backgroundColor: "red",
+      }}
+    ></div>
   );
-}
+};
 
 export default AnimatedRectangle;
