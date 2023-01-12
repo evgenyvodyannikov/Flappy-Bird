695a52df5b65a2e383f731cf2aa4f2e4acedb3b6
Evgeny Vodyannikov <vodyannikov.eugene@gmail.com>
2023-01-12 19:41:23 +0500

    update UI with 60 fps

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,26 @@
-import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import React, { useState, useEffect } from 'react';
 
-function App() {
+const App: React.FC = () => {
+  const [frame, setFrame] = useState(0);
+
+  useEffect(() => {
+    let animationFrameId: number;
+    let currentFrame = 0;
+
+    function updateFrame() {
+      setFrame(currentFrame);
+      currentFrame++;
+      animationFrameId = requestAnimationFrame(updateFrame);
+    }
+
+    animationFrameId = requestAnimationFrame(updateFrame);
+
+    return () => cancelAnimationFrame(animationFrameId);
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      Frame: {frame}
     </div>
   );
 }
