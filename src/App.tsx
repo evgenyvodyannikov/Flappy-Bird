f74aa4a1d6de247a5a5e8ce38438cde807cd01b4
evgenyvodyannikov <vodyannikov.eugene@gmail.com>
2023-01-13 13:02:38 +0500

    add new background and test switch

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,40 @@
-import AnimatedRectangle from './components/AnimatedRectangle'
+import React, { useState, useEffect, useCallback } from "react";
+
+import AnimatedRectangle from './components/Player'
 import MovingBackground from './components/MovingBackground'
 import BackgroundImage from './assets/images/background.png'
+import BackgroundImage1 from './assets/images/background1.png'
+import BackgroundImage2 from './assets/images/background2.jpg'
 
 const App: React.FC = () => {
+
+  const [currentBackground, setCurrentBackground] = useState<string>(BackgroundImage);
+
+  const handleKeyDown = useCallback((event: KeyboardEvent) => {
+    switch (event.key) {
+      case "c":
+        setCurrentBackground(BackgroundImage1)
+        console.log("handled");
+        break;
+      case "v":
+        setCurrentBackground(BackgroundImage2)
+        break;
+      default:
+        break;
+    }
+  }, []);
+
+
+  useEffect(() => {
+
+    document.addEventListener("keydown", handleKeyDown);
+
+  }, [handleKeyDown]);
+
   return (
     <div >
-      <MovingBackground imageUrl={BackgroundImage} />
-      <AnimatedRectangle x={0} y={0} width={100} height={100} />
+      <MovingBackground imageUrl={currentBackground} />
+      <AnimatedRectangle x={0} y={0} width={75} height={75} />
     </div>
     
   )
