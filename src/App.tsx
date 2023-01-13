6ab2bde7aae11ba55c6db29edac71cdd49a75423
evgenyvodyannikov <vodyannikov.eugene@gmail.com>
2023-01-13 06:54:07 +0500

    add moving background

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
 import AnimatedRectangle from './components/AnimatedRectangle'
+import MovingBackground from './components/MovingBackground'
+import BackgroundImage from './assets/images/background.png'
 
 const App: React.FC = () => {
   return (
-    <AnimatedRectangle x={0} y={0} width={100} height={100} />
+    <div >
+      <MovingBackground imageUrl={BackgroundImage} />
+      <AnimatedRectangle x={0} y={0} width={100} height={100} />
+    </div>
+    
   )
 }
 
---
+++ src/components/MovingBackground.tsx
@@ -0,0 +1,35 @@
+import React, { useEffect, useRef } from 'react';
+
+interface Props {
+    imageUrl: string;
+}
+
+const MovingBackground: React.FC<Props> = ({ imageUrl }) => {
+    const containerRef = useRef<HTMLDivElement>(null);
+
+    useEffect(() => {
+        const container = containerRef.current;
+        let xPos = 0;
+        let animationFrameId: number;
+
+        const moveBackground = () => {
+            xPos -= 1;
+            container!.style.backgroundPosition = `${xPos}px 0`;
+            animationFrameId = requestAnimationFrame(moveBackground);
+        }
+        moveBackground();
+        return () => cancelAnimationFrame(animationFrameId);
+    }, []);
+
+    return (
+        <div ref={containerRef} className="full" style={{
+            width: '100%',
+            height: '100vh',
+            backgroundImage: `url(${imageUrl})`,
+            backgroundRepeat: 'repeat-x',
+        }}></div>
+        
+    );
+};
+
+export default MovingBackground;
